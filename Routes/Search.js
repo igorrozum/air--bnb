4f5dfaddb547760f3e86cb81ca0a49ffb1b63f9a
Igor Rozum <irozum@greengeeks.com>
2019-11-26 18:48:30 -0500

    search by title and address added

--- Routes/Search.js
+++ Routes/Search.js
@@ -2,9 +2,13 @@ const express = require('express')
 const router = express.Router()
 const Room = require('../Models/Room')
 
+
+
 router.post('/', (req, res) => {
-    // Room.find({city: req.body.cityToSearch})
-    Room.find({city: { $regex : new RegExp(req.body.cityToSearch, "i") }})
+    Room.find({$or:[{city: { $regex : new RegExp(req.body.cityToSearch, "i") }},
+                    {title:{ $regex : new RegExp(req.body.cityToSearch, "i") }},
+                    {address:{ $regex : new RegExp(req.body.cityToSearch, "i") }},
+                    {description:{ $regex : new RegExp(req.body.cityToSearch, "i") }}]})
     .then(rooms => {
         res.render('listings', {
             rooms: rooms
@@ -12,7 +16,6 @@ router.post('/', (req, res) => {
     })
     
 })
-
 
 
 
